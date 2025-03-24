8cc9e375db41a0be76eaf45fac13c9fbc9ea1f32
nahanaoo <ericsven.u@gmail.com>
2025-03-24 14:32:27 +0100

    parallized simulations and made them cluster runnable

--- simulations.py
+++ simulations.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+#
+# Reserve 1 CPUs for this job
+#
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=1G
+#
+# Request it to run this for DD:HH:MM with ?G per core
+#
+#SBATCH --time=00:10:00
+#
+import sys
+
+sys.path.append("/users/eulrich/toxicity_kuleuven")
 from model import Experiment
 import numpy as np
 from scipy.integrate import odeint
@@ -67,54 +81,31 @@ def two_sided_protection():
     e.plot_chloram("two_sided_protection/chloram")
 
 
-def run_experiment(rE, rS):
-    e = Experiment()
-    e.E.r = rE
-    e.S.r = rS
-    e.total_transfers = 50
-    e.transfer()
-    return e.E.N[-1] / (e.E.N[-1] + e.S.N[-1])
-
-
-def co_existence_s_protects_e():
-    start = timer()
-    rs = np.linspace(0, 1.5, 100)
-    zs = np.zeros((len(rs), len(rs)))
-
-    # Parallel computation
-    results = Parallel(n_jobs=-1)(
-        delayed(run_experiment)(rE, rS) for rE in rs for rS in rs
-    )
-
-    # Reshape back to matrix form
-    zs = np.array(results).reshape(len(rs), len(rs))
-    stop = timer()
-    print(stop - start)
-
+def plot_competition(rE, rS, zs,title):
     fig = go.Figure()
     fig.add_trace(
         go.Contour(
-            x=rs,
-            y=rs,
+            x=rE,
+            y=rS,
             z=zs,
             colorscale=coolwarm_colorscale,
             colorbar=dict(
                 title="Relative abundance <i>E. coli</i>",
                 title_side="right",
-                dtick=0.2,
+                dtick=0.1,
                 tickfont=dict(size=font_size),
             ),
-            contours=dict(showlines=False, start=0, end=1, size=0.2),
+            contours=dict(showlines=False, start=0, end=1, size=0.1),
         )
     )
     fig.update_layout(
         xaxis=dict(
-            range=[0, max(rs)],
+            range=[0, max(rE)],
             dtick=0.2,
             title="Maximum growth rate <i>E. coli</i> [1/h]",
         ),
         yaxis=dict(
-            range=[0, max(rs)],
+            range=[0, max(rS)],
             dtick=0.2,
             title="Maximum growth rate <i>Salmonella</i> [1/h]",
         ),
@@ -123,4 +114,78 @@ def co_existence_s_protects_e():
         height=height,
     )
     fig = style_plot(fig, line_thickness=1, left_margin=50)
+    return fig
+
+
+def competition_simulations():
+    def no_antibiotics(rE, rS):
+        e = Experiment()
+        e.M_cefo = 0
+        e.M_chloram = 0
+        e.E.r = rE
+        e.S.r = rS
+        e.total_transfers = 50
+        e.transfer()
+        return e.E.N[-1] / (e.E.N[-1] + e.S.N[-1])
+
+    def s_protects_e(rE, rS):
+        e = Experiment()
+        e.M_chloram = 0
+        e.E.r = rE
+        e.S.r = rS
+        e.total_transfers = 50
+        e.transfer()
+        return e.E.N[-1] / (e.E.N[-1] + e.S.N[-1])
+
+    def e_protects_s(rE, rS):
+        e = Experiment()
+        e.M_cefo = 0
+        e.E.r = rE
+        e.S.r = rS
+        e.total_transfers = 50
+        e.transfer()
+        return e.E.N[-1] / (e.E.N[-1] + e.S.N[-1])
+
+    def two_sided_protection(rE, rS):
+        e = Experiment()
+        e.E.r = rE
+        e.S.r = rS
+        e.total_transfers = 50
+        e.transfer()
+        return e.E.N[-1] / (e.E.N[-1] + e.S.N[-1])
+
+    rs = np.linspace(0, 1.5, 100)
+    zs = np.zeros((len(rs), len(rs)))
+    results = Parallel(n_jobs=-1)(
+        delayed(no_antibiotics)(rE, rS) for rE in rs for rS in rs
+    )
+    zs = np.array(results).reshape(len(rs), len(rs))
+    fig = plot_competition(rs, rs, zs,"No antibiotics")
+    fig.write_image("plots/coexistence/no_antibiotics/growth_rates.svg")
+
+    zs = np.zeros((len(rs), len(rs)))
+    results = Parallel(n_jobs=-1)(
+        delayed(s_protects_e)(rE, rS) for rE in rs for rS in rs
+    )
+    zs = np.array(results).reshape(len(rs), len(rs))
+    fig = plot_competition(rs, rs, zs,"<i>Salmonella</i> protects <i>E. coli</i>")
     fig.write_image("plots/coexistence/s_protects_e/growth_rates.svg")
+
+    zs = np.array(results).reshape(len(rs), len(rs))
+    results = Parallel(n_jobs=-1)(
+        delayed(e_protects_s)(rE, rS) for rE in rs for rS in rs
+    )
+    zs = np.array(results).reshape(len(rs), len(rs))
+    fig = plot_competition(rs, rs, zs,"<i>E. coli</i> protects <i>Salmonella</i>")
+    fig.write_image("plots/coexistence/e_protects_s/growth_rates.svg")
+
+    zs = np.array(results).reshape(len(rs), len(rs))
+    results = Parallel(n_jobs=-1)(
+        delayed(two_sided_protection)(rE, rS) for rE in rs for rS in rs
+    )
+    zs = np.array(results).reshape(len(rs), len(rs))
+    fig = plot_competition(rs, rs, zs,"Two sided protection")
+    fig.write_image("plots/coexistence/two_sided/growth_rates.svg")
+
+
+competition_simulations()
